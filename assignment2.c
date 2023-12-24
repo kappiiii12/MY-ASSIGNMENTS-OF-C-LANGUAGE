da1567d517f0c82b0a44d65940b2dfe5d8c64404
kapil kumar <154614224+kappiiii12@users.noreply.github.com>
2023-12-24 20:44:29 +0530

    Add files via upload

---
+++ assignment2.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+int main()
+//question 5 profit or loss
+/*
+{
+    int cp,sp;
+    printf("enter the selling price per dozen = ");
+    scanf("%d",&sp);
+     printf("enter the cost price per dozen = ");
+    scanf("%d",&cp);
+    if(cp>sp)
+     printf("loss upon selling 25 bananas = %d",((cp-sp)/12)*25);
+    else
+    printf("profit upon selling 25 bananas = %d",((sp-cp)/12)*25);
+    return 0;
+    
+}
+*/
+//question 4 volume of cuboid
+/*
+{
+    int l,b,h;
+    printf("enter lenght = ");
+    scanf("%d",&l);
+    printf("enter width = ");
+    scanf("%d",&b);
+    printf("enter height = ");
+    scanf("%d",&h);
+    printf("volume od cuboid =%d ",l*b*h);
+    return 0;
+}
+*/
+//question 3 calculate simple interest
+/*
+{
+    float P,T,R;
+     printf("enter the price = ");
+     scanf("%f",&P);
+      printf("enter the time period = ");
+      scanf("%f",&T);
+       printf("enter the rate = ");
+       scanf("%f",&R);
+        printf("simple interest = %f",(P*T*R)/100);
+        return 0;
+}
+*/
+//question 2 circumference of circle
+/*
+{
+    float r;
+    printf("enter the radius of circle = ");
+    scanf("%f",&r);
+    printf("circumference = %f",6.28*r);//2*3.14=6.28
+    return 0;
+}
+*/
+//queation 1 average of three numbers given by user
+/*
+{
+  float n1,n2,n3;
+    printf("enter the value of n1= "); 
+    scanf("%f",&n1);
+     printf("enter the value of n2= "); 
+     scanf("%f",&n2);
+      printf("enter the value of n3= "); 
+      scanf("%f",&n3);
+      printf("average= %f",(n1+n2+n3)/3);
+      return 0;
+}
+*/
