b8f5bc96cda2e4bf58110ac076cc3fc531b675e7
kapil kumar <154614224+kappiiii12@users.noreply.github.com>
2023-12-24 11:21:00 +0530

    Add files via upload

---
+++ assignment1.c
@@ -0,0 +1,90 @@
+
+#include<stdio.h>
+int main()
+//question 10 
+/*
+{
+    float r;
+    printf("value of radius");
+    scanf("%f",&r);
+    printf("%f",3.14*r*r);
+    return 0;
+}
+*/
+//question 9
+/*
+{
+    int l,w;
+    printf("enter length and width");
+    scanf("%d%d",&l,&w);
+    printf("area=%d",l*w);
+    return 0;
+}
+*/
+//question 8
+/*
+{
+    int a;
+    printf("enter a number");
+    scanf("%d",&a);
+    printf("%d",a*a);
+    return 0;
+}
+*/
+//question 7
+/*
+{
+    int a,b;
+    printf("enter two numbers");
+    scanf("%d%d",&a,&b);
+    printf("sum=%d",a+b);
+    return 0;
+}
+*/
+// question 6
+/*
+{
+    printf("\"teacher's day\"");
+    return 0;
+}
+*/
+//question 5
+/*
+{
+    printf("\\r");
+    return 0;
+}
+*/
+//question 4
+/*
+{
+    printf("\\n");
+    return 0;
+}
+*/
+//question 3
+/*
+{
+    printf("\"mysirg\"");
+    return 0;
+}
+*/
+//question 2
+/*
+int main()
+{
+    printf("hello\nstudents");
+    return 0;
+}
+*/
+
+//question 1 
+/*
+int main()
+{
+    printf("hello");
+    return 0;
+    getch()
+}
+*/
+
