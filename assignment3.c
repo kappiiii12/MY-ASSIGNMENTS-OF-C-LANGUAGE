72b89c4d05716b6fcb9d1ff955714997660831a7
kapil kumar <154614224+kappiiii12@users.noreply.github.com>
2023-12-25 13:42:31 +0530

    Add files via upload

---
+++ assignment3.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+//question 1 Write a program to input a character from the user and print its ASCII code.
+/*
+{
+    char a;
+    printf("enter a character = ");
+    scanf("%c",&a);
+    printf("ASCII code = %d",a);
+    return 0;
+
+}
+//question 2 Write a program to input an ASCII code from the user and print its corresponding character.
+{
+   int a;
+    printf("enter ASCII code = ");
+    scanf("%d",&a);
+    printf("character =%c",a);
+    return 0;
+}
+//question 3 Write a program to input three characters from the user and display characters with their corresponding ASCII codes.
+{
+    char a,b,c;
+    printf("input three character\n(space is also a character)\n--");
+    scanf("%c%c%c",&a,&b,&c);
+    printf("%c=%d,%c=%d,%c=%d",a,a,b,b,c,c);
+    return 0;
+}
+//question 4 Write a program to print last digit of a given number.
+{
+    int a;
+    printf("enter any number = ");
+    scanf("%d",&a);
+    printf("last digit of number is = %d",a%10);
+    return 0;
+}
+//question 5 Write a program to print a given number without its last digit.
+{
+    int a;
+    printf("enter any number = ");
+    scanf("%d",&a);
+    printf("number without last digit is = %d",a/10);
+    return 0;
+}
+*/
