3d6678b639ab3b1bad007ebfb65414c2af6afde4
kapil kumar <154614224+kappiiii12@users.noreply.github.com>
2023-12-26 21:47:31 +0530

    Add files via upload

---
+++ assignment11.c
@@ -0,0 +1,143 @@
+/*
+1. Write a program to print MySirG 5 times on the screen
+
+2. Write a program to print the first 10 natural numbers.
+
+3. Write a program to print the first 10 natural numbers in reverse order
+
+4. Write a program to print the first 10 odd natural numbers
+
+5. Write a program to print the first 10 odd natural numbers in reverse order.
+
+6. Write a program to print the first 10 even natural numbers
+
+7. Write a program to print the first 10 even natural numbers in reverse order
+
+8. Write a program to print squares of the first 10 natural numbers
+
+9. Write a program to print cubes of the first 10 natural numbers
+
+10. Write a program to print a table of 5.
+*/
+#include<stdio.h>
+int main()
+//question 1
+/*
+{
+   int i=1;
+   while(i<6)
+   {
+      printf("mysirg\n");
+      i++;
+   }
+   return 0;
+
+}
+*/
+//question 2
+/*
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("%d\n",i);
+    i++;
+    }
+    return 0;
+}
+*/
+//question 3
+/*
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("%d\n",11-i);
+    i++;
+    }
+    return 0;
+}
+*/
+//question 4
+/*
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("%d\n",2*i-1);
+    i++;
+    }
+    return 0;
+}
+*/
+//question 4
+/*
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("%d\n",20-(2*i-1));
+    i++;
+    }
+    return 0;
+}*/
+//question 6
+/*
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("%d\n",2*i);
+    i++;
+    }
+    return 0;
+}
+*/
+//question 7
+/*
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("%d\n",22-(2*i));
+    i++;
+    }
+    return 0;
+}
+*/
+//question 8
+/*
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("%d\n",i*i);
+    i++;
+    }
+    return 0;
+}
+*/
+//question 9
+/*
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("%d\n",i*i*i);
+    i++;
+    }
+    return 0;
+}
+*/
+//question 10
+/*
+{
+    int i=1;
+    while(i<=10)
+    {
+    printf("5*%d=%d\n",i,5*i);
+    i++;
+    }
+    return 0;
+}
+*/
