a08b8ce8774fb1964121fc961c81a7fd2889f737
kapil kumar <154614224+kappiiii12@users.noreply.github.com>
2023-12-27 09:35:29 +0530

    Add files via upload

---
+++ assignment13.c
@@ -0,0 +1,88 @@
+/*
+1. Write a program to calculate sum of first N natural numbers
+
+2. Write a program to calculate sum of first N even natural numbers
+
+3. Write a program to calculate sum of first N odd natural numbers
+
+4. Write a program to calculate sum of squares of first N natural numbers
+
+5. Write a program to calculate sum of cubes of first N natural numbers
+*/
+#include<stdio.h>
+int main()
+//question 1
+/*
+{
+    int n,i=1,sum=0;
+    printf("enter the value of n = ");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+    sum=sum+i;
+    i++;
+    }
+    printf("sum =%d",sum);
+    return 0;
+}
+*/
+//question 2
+/*
+{
+    int n,i=1,sum=0;
+    printf("enter the value of n = ");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+    sum=sum+2*i;
+    i++;
+    }
+    printf("sum =%d",sum);
+    return 0;
+}
+*/
+//question 3
+/*
+{
+    int n,i=1,sum=0;
+    printf("enter the value of n = ");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+    sum=sum+(2*i-1);
+    i++;
+    }
+    printf("sum =%d",sum);
+    return 0;
+}
+*/
+//question 4
+/*
+{
+    int n,i=1,sum=0;
+    printf("enter the value of n = ");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+    sum=sum+i*i;
+    i++;
+    }
+    printf("sum =%d",sum);
+    return 0;
+}
+*/
+//question 5
+/*
+{
+    int n,i=1,sum=0;
+    printf("enter the value of n = ");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+    sum=sum+i*i*i;
+    i++;
+    }
+    printf("sum =%d",sum);
+    return 0;
+}
+*/
